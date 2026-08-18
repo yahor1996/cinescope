ecbdeda9ba2955780ef906444c7ac442449515ce
yahor1996 <EgorGromovYur@yandex.ru>
2026-08-18 16:10:06 +0300

    changed environment variables

--- resources/user_creds.py
+++ resources/user_creds.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class SuperAdminCreds:
-    USERNAME = "api1@gmail.com"
-    PASSWORD = "asdqwe123Q"
+    USERNAME = os.getenv("SUPER_ADMIN_USERNAME")
+    PASSWORD = os.getenv("SUPER_ADMIN_PASSWORD")
